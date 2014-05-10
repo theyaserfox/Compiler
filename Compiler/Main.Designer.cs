d9e471f3bf2f57919cb6a55c298d3f740fd49ce9
theyaserfox <yaser.omar.abdelaziz@gmail.com>
2014-05-10 10:06:01 +0200

    Change interface and enhance scanning phase

--- Compiler/Main.Designer.cs
+++ Compiler/Main.Designer.cs
@@ -30,10 +30,17 @@
         {
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.компілюватиToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.tbProgram = new System.Windows.Forms.TextBox();
             this.tbCompiled = new System.Windows.Forms.TextBox();
+            this.tbProgram = new System.Windows.Forms.TextBox();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.literalsView = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.constantsView = new System.Windows.Forms.ListView();
+            this.Constants = new System.Windows.Forms.Label();
+            this.symbolsView = new System.Windows.Forms.ListView();
+            this.label2 = new System.Windows.Forms.Label();
             this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
@@ -57,44 +64,105 @@
             this.компілюватиToolStripMenuItem.Text = "Компілювати";
             this.компілюватиToolStripMenuItem.Click += new System.EventHandler(this.компілюватиToolStripMenuItem_Click);
             // 
+            // tbCompiled
+            // 
+            this.tbCompiled.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbCompiled.Location = new System.Drawing.Point(3, 3);
+            this.tbCompiled.Multiline = true;
+            this.tbCompiled.Name = "tbCompiled";
+            this.tbCompiled.Size = new System.Drawing.Size(670, 112);
+            this.tbCompiled.TabIndex = 0;
+            // 
+            // tbProgram
+            // 
+            this.tbProgram.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbProgram.Location = new System.Drawing.Point(3, 3);
+            this.tbProgram.Multiline = true;
+            this.tbProgram.Name = "tbProgram";
+            this.tbProgram.Size = new System.Drawing.Size(670, 154);
+            this.tbProgram.TabIndex = 0;
+            // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.Location = new System.Drawing.Point(0, 24);
             this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
             // 
             // splitContainer1.Panel1
             // 
+            this.splitContainer1.Panel1.Controls.Add(this.label2);
+            this.splitContainer1.Panel1.Controls.Add(this.Constants);
+            this.splitContainer1.Panel1.Controls.Add(this.label1);
+            this.splitContainer1.Panel1.Controls.Add(this.symbolsView);
+            this.splitContainer1.Panel1.Controls.Add(this.constantsView);
+            this.splitContainer1.Panel1.Controls.Add(this.literalsView);
             this.splitContainer1.Panel1.Controls.Add(this.tbProgram);
+            this.splitContainer1.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No;
             // 
             // splitContainer1.Panel2
             // 
             this.splitContainer1.Panel2.Controls.Add(this.tbCompiled);
+            this.splitContainer1.Panel2.RightToLeft = System.Windows.Forms.RightToLeft.No;
             this.splitContainer1.Size = new System.Drawing.Size(676, 440);
             this.splitContainer1.SplitterDistance = 318;
             this.splitContainer1.TabIndex = 1;
             // 
-            // tbProgram
+            // literalsView
             // 
-            this.tbProgram.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbProgram.Location = new System.Drawing.Point(3, 3);
-            this.tbProgram.Multiline = true;
-            this.tbProgram.Name = "tbProgram";
-            this.tbProgram.Size = new System.Drawing.Size(312, 434);
-            this.tbProgram.TabIndex = 0;
+            this.literalsView.Cursor = System.Windows.Forms.Cursors.Default;
+            this.literalsView.Location = new System.Drawing.Point(3, 185);
+            this.literalsView.Name = "literalsView";
+            this.literalsView.Size = new System.Drawing.Size(121, 130);
+            this.literalsView.TabIndex = 1;
+            this.literalsView.UseCompatibleStateImageBehavior = false;
             // 
-            // tbCompiled
+            // label1
             // 
-            this.tbCompiled.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tbCompiled.Location = new System.Drawing.Point(3, 3);
-            this.tbCompiled.Multiline = true;
-            this.tbCompiled.Name = "tbCompiled";
-            this.tbCompiled.Size = new System.Drawing.Size(348, 434);
-            this.tbCompiled.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Literals";
+            // 
+            // constantsView
+            // 
+            this.constantsView.Location = new System.Drawing.Point(130, 185);
+            this.constantsView.Name = "constantsView";
+            this.constantsView.Size = new System.Drawing.Size(121, 130);
+            this.constantsView.TabIndex = 1;
+            this.constantsView.UseCompatibleStateImageBehavior = false;
+            // 
+            // Constants
+            // 
+            this.Constants.AutoSize = true;
+            this.Constants.Location = new System.Drawing.Point(163, 164);
+            this.Constants.Name = "Constants";
+            this.Constants.Size = new System.Drawing.Size(54, 13);
+            this.Constants.TabIndex = 2;
+            this.Constants.Text = "Constants";
+            // 
+            // symbolsView
+            // 
+            this.symbolsView.Location = new System.Drawing.Point(257, 185);
+            this.symbolsView.Name = "symbolsView";
+            this.symbolsView.Size = new System.Drawing.Size(121, 130);
+            this.symbolsView.TabIndex = 1;
+            this.symbolsView.UseCompatibleStateImageBehavior = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(293, 164);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Symbols";
             // 
             // Main
             // 
@@ -112,6 +180,7 @@
             this.splitContainer1.Panel1.PerformLayout();
             this.splitContainer1.Panel2.ResumeLayout(false);
             this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -122,10 +191,14 @@
 
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem компілюватиToolStripMenuItem;
-        private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.TextBox tbProgram;
         private System.Windows.Forms.TextBox tbCompiled;
-
+        private System.Windows.Forms.TextBox tbProgram;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListView literalsView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Constants;
+        private System.Windows.Forms.ListView constantsView;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView symbolsView;
     }
 }
-
--- Compiler/Main.cs
+++ Compiler/Main.cs
@@ -18,9 +18,295 @@ namespace Compiler
 
         private void компілюватиToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            var compiler = new Compiler();
-            tbCompiled.Text = compiler.Compile(tbProgram.Text);
-            MessageBox.Show(compiler.Errors.Aggregate("", (current, error) => current + (error + Environment.NewLine)));
+            tbCompiled.Text = Compile(tbProgram.Text);
+            tbCompiled.Text = Errors.Aggregate("", (current, error) => current + (error + Environment.NewLine));
+        }
+
+        List<String> declared = new List<String>();
+        List<String> initialized = new List<String>();
+        List<String> startinit = new List<String>();
+        private String initialization = "";
+        private String code = @".code
+start:
+";
+
+        public List<String> Errors = new List<String>();
+        String compiled = @".586
+.model flat,stdcall
+option casemap:none            
+include \masm32\include\windows.inc
+include \masm32\include\masm32.inc
+include \masm32\include\kernel32.inc
+include \masm32\include\gdi32.inc
+include \masm32\include\user32.inc
+include \masm32\include\debug.inc
+includelib \masm32\lib\masm32.lib
+includelib \masm32\lib\kernel32.lib
+includelib \masm32\lib\gdi32.lib
+includelib \masm32\lib\user32.lib
+includelib \masm32\lib\debug.lib      
+DBGWIN_EXT_INFO = 0
+.data
+";
+        public String Compile(String program)
+        {
+            IEnumerable<string> commands = Commands(program);
+            foreach (var tokens in commands.Select(Parce))
+            {
+                #region show tokens
+                string show = "";
+                for (int i = 0; i < tokens.Count(); i++)
+                    show += tokens[i] + " ";
+                MessageBox.Show(show);
+                #endregion
+
+                #region scanning
+                literalsView.Items.Clear();
+                constantsView.Items.Clear();
+                symbolsView.Items.Clear();
+                for (int i = 0; i < tokens.Count(); i++)
+                {
+                    if (IsVariable(tokens[i]))
+                        literalsView.Items.Add(tokens[i]);
+                    else if (IsNumber(tokens[i]))
+                        constantsView.Items.Add(tokens[i]);
+                    else if (IsSymbol(tokens[i]))
+                        symbolsView.Items.Add(tokens[i]);
+                }
+                #endregion
+
+                if (IsVariable(tokens[0]) && (tokens.Count() == 1))
+                {
+                    if (declared.Contains(tokens[0]))
+                    {
+                        Errors.Add("Variable " + tokens[0] + " already declared");
+                    }
+                    else
+                    {
+                        declared.Add((tokens[0]));
+                    }
+                }
+                else if ((IsVariable(tokens[0])) && (tokens[1] == "=") && (IsNumber(tokens[2])))
+                {
+                    if (declared.Contains(tokens[0]))
+                    {
+                        if (!initialized.Contains(tokens[0]))
+                        {
+                            initialization += tokens[0] + " DW " + tokens[2] + Environment.NewLine;
+                            initialized.Add(tokens[0]);
+                            startinit.Add(tokens[0]);
+                        }
+                        else
+                        {
+                            Errors.Add("Variable " + tokens[0] + " already initialized");
+                        }
+                    }
+                    else
+                    {
+                        Errors.Add("Variable " + tokens[0] + " not declared");
+                    }
+                }
+                else if ((IsVariable(tokens[0])) && (tokens[1] == "=") && (IsExpresion(tokens[2])))
+                {
+                    code += ParceExpresion(tokens[2]);
+                    code += "POP AX" + Environment.NewLine;
+                    code += "MOV " + tokens[0] + ",AX" + Environment.NewLine;
+                    initialized.Add(tokens[0]);
+                }
+                else
+                {
+                    Errors.Add("Unknown command (Error in expression)");
+                }
+            }
+
+            foreach (var variable in declared.Where(variable => !startinit.Contains(variable)))
+            {
+                initialization += variable + " DW 0" + Environment.NewLine;
+            }
+
+            compiled += initialization;
+            compiled += code;
+
+            compiled += declared.Aggregate("", (current, variable) => current + ("DumpMem offset " + variable + ", 1" + Environment.NewLine));
+            compiled += @"invoke ExitProcess, NULL
+end start
+end";
+            compiled = Optimize(compiled);
+            return compiled;
+        }
+
+        private string Optimize(string program)
+        {
+            return program.Replace("PUSH AX\r\nPOP AX\r\n", "");
+        }
+
+        private string ParceExpresion(string token)
+        {
+            if (IsVariable(token))
+                if (initialized.Contains(token))
+                {
+                    var temp = "";
+                    temp += "MOV AX," + token + Environment.NewLine;
+                    temp += "PUSH AX" + Environment.NewLine;
+                    return temp;
+                }
+                else
+                {
+                    Errors.Add("Variable " + token + " not initialized");
+                    return "";
+                }
+            if (IsNumber(token))
+            {
+                var temp = "";
+                temp += "MOV AX," + token + Environment.NewLine;
+                temp += "PUSH AX" + Environment.NewLine;
+                return temp;
+            }
+            var posp = token.IndexOf('+');
+            var posb = token.IndexOf('(');
+            if ((posb == -1) && (posp == -1)) return "";
+            if (posb < 0) posb = int.MaxValue;
+            if (posp < 0) posp = int.MaxValue;
+            if (posp < posb)
+            {
+                var temp = "";
+                temp += ParceExpresion(token.Substring(0, posp));
+                temp += ParceExpresion(token.Substring(posp + 1, token.Length - posp - 1));
+                temp += "POP AX" + Environment.NewLine;
+                temp += "POP BX" + Environment.NewLine;
+                temp += "ADD AX,BX" + Environment.NewLine;
+                temp += "PUSH AX" + Environment.NewLine;
+                return temp;
+            }
+            if (posb != 0) return "";
+            var p = 1;
+            if (posb < posp)
+            {
+                int i;
+                for (i = 1; i < token.Length; i++)
+                {
+                    if (token[i] == '(') p++;
+                    if (token[i] == ')') p--;
+                    if (p == 0) break;
+                }
+                if (i + 1 == token.Length)
+                {
+                    var temp = "";
+                    temp += ParceExpresion(token.Substring(posb + 1, i - 1));
+                    return temp;
+                }
+                else if (token[i + 1] == '+')
+                {
+                    var temp = "";
+                    temp += ParceExpresion(token.Substring(posb + 1, i - 1));
+                    temp += ParceExpresion(token.Substring(i + 2, token.Length - i - 2));
+                    temp += "POP AX" + Environment.NewLine;
+                    temp += "POP BX" + Environment.NewLine;
+                    temp += "ADD AX,BX" + Environment.NewLine;
+                    temp += "PUSH AX" + Environment.NewLine;
+                    return temp;
+                }
+            }
+            return "";
+        }
+
+        private bool IsExpresion(string token)
+        {
+            if (IsVariable(token))
+                if (initialized.Contains(token))
+                    return true;
+                else
+                {
+                    Errors.Add("Variable " + token + " not initialized");
+                    return false;
+                }
+            if (IsNumber(token)) return true;
+            var posp = token.IndexOf('+');
+            var posb = token.IndexOf('(');
+            if ((posb == -1) && (posp == -1)) return false;
+            if (posb < 0) posb = int.MaxValue;
+            if (posp < 0) posp = int.MaxValue;
+            if (posp < posb)
+            {
+                return IsExpresion(token.Substring(0, posp)) &&
+                       IsExpresion(token.Substring(posp + 1, token.Length - posp - 1));
+            }
+            if (posb != 0) return false;
+            var p = 1;
+            if (posb < posp)
+            {
+                int i;
+                for (i = 1; i < token.Length; i++)
+                {
+                    if (token[i] == '(') p++;
+                    if (token[i] == ')') p--;
+                    if (p == 0) break;
+                }
+                if (p != 0) return false;
+
+                if (i + 1 == token.Length)
+                {
+                    return IsExpresion(token.Substring(posb + 1, i - 1));
+                }
+                else if (token[i + 1] == '+')
+                {
+                    return IsExpresion(token.Substring(posb + 1, i - 1)) &&
+                           IsExpresion(token.Substring(i + 2, token.Length - i - 2));
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        private bool IsNumber(String token)
+        {
+            return token.All(c => (c >= '0') && (c <= '9'));
+        }
+
+        private bool IsVariable(String token)
+        {
+            return token.All(c => ((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')));
+        }
+
+        private bool IsSymbol(String token)
+        {
+            switch (token)
+            {
+                case ";":
+                case ",":
+                case "=":
+                    return true;
+            }
+            return false;
+        }
+
+        private String[] Parce(String command)
+        {
+            if (command.IndexOf('=') == -1)
+            {
+                return new[] { command };
+            }
+            for (int i = 0; i < command.Count() - 2; i++)
+            {
+                if (((!char.IsLetter(command[i]) || !char.IsLetter(command[i + 2]))) && (command[i + 1] == ' '))
+                {
+                    command = command.Remove(i + 1, 1);
+                }
+            }
+            return new[] { command.Substring(0, command.IndexOf('=')), "=", command.Substring(command.IndexOf('=') + 1, command.Length - command.IndexOf('=') - 1) };
+        }
+
+        private List<string> Commands(String program)
+        {
+            var separator = new[] { ";" };
+            program.Replace("\r\n", "");
+            var commands = new List<String>(program.Split(separator, StringSplitOptions.RemoveEmptyEntries).AsEnumerable());
+            for (var i = 0; i < commands.Count; i++)
+                commands[i] = commands[i].Trim().Replace("\r\n", "");
+            return commands;
         }
     }
 }
